e10b6ad5c6b40d4509cbcc494f411b646ba635a6
wilderincon <wilder.rincon@bitsamericas.com>
2018-08-15 20:38:17 -0500

    Add new change

--- Controllers/controller.php
+++ Controllers/controller.php
@@ -3,9 +3,21 @@
 class MvcController{
 
 function ConsultarFichaController(){
-
+$datosModel = '*';
+$respuesta = Model::consultarFichaModel($datosModel, 'rol');
 echo "Hola mundo1";
-include "Views/view_ficha.php";
+//include "Views/view_ficha.php";
+var_dump ($respuesta);
+
+}
+
+
+function ConsultarFichaControllerSigleton(){
+$datosModel = '*';
+$respuesta = ModelConection::ConsultarFichaModelSigleton($datosModel, 'rol');
+echo "Hola mundo1";
+//include "Views/view_ficha.php";
+var_dump ($respuesta);
 
 }
 
