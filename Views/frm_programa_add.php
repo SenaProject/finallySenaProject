427ef3e2cfa15a8ad3e027e73a16597336360b1a
wilderincon <wilder.rincon@bitsamericas.com>
2018-09-28 19:44:17 -0500

    new style 28092018

--- Views/frm_programa_add.php
+++ Views/frm_programa_add.php
@@ -3,22 +3,30 @@
   <head>
     <meta charset="utf-8">
     <title></title>
+    <link rel="stylesheet" type="text/css" href="./css/programaCrear.css">
   </head>
   <body>
 <h2>Nuevo Programa</h2>
 <form class="" action="../Controllers/valida_Programa.php?valor='crear'" method="post">
-  <?php
+  <div class="content_program">
+   <div class="subcontent_program">
+     <?php
 
-  echo "Nombre del Programa:";
-  echo "  <input type='text' name='nombre_programa' value=''> <br>";
-  echo "Estado:";
-  echo "  <input type='text' name='estado_programa' value=''> <br>";
-  echo "<br><br>";
-  echo "  <input type='submit' name='BtnGuardar' value='Guardar'>";
-  echo "  <input type='reset' name='BtnLimpiar' value='Borrar'>";
+     echo "<div class='title_programa'>Nombre del Programa: </div>";
+     echo "<div class='field_program'>  <input  type='text' name='nombre_programa' value=''> </div>";
+     echo "<div class='title_programa'>Estado:</div>";
+     echo " <div class='field_program'> <input type='text' name='estado_programa' value=''> </div>";
+     echo "<br><br>";
+     echo " <div class='button_guardar'> <input type='submit' name='BtnGuardar' value='Guardar'></div>";
+     echo "  <div class='button_reset'><input type='reset' name='BtnLimpiar' value='Borrar'></div>";
 
-   ?>
-</form>
+      ?>
+     </form>
+   </div>
+
+  </div>
+
+
 <h2><a href="frm_programa.php">Volver ...</a></h2>
 
   </body>
